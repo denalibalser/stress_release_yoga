6eb4e9a5ab8ab24578635ffe068f61abc998f977
Denali Balser <denali.balser167@gmail.com>
2019-06-11 13:41:37 +0000

    added to yoga_poses.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,13 +1,14 @@
 class CLI 
   
   def start 
+    puts "Welcome. Learning how to becoming a software engineer can be very stressful, not to mention life in general. It is very important to take care of your mind and body in times of stress, which is usually when you feel the least like doing so. Yoga is a great way to both destress the mind and move the body after hours of sitting at the computer learning how to code."
     list_poses
     select_pose
     goodbye
+    @poses = YogaPose.all
   end 
   
   def list_poses
-    puts "Welcome. Learning how to becoming a software engineer can be very stressful, not to mention life in general. It is very important to take care of your mind and body in times of stress, which is usually when you feel the least like doing so. Yoga is a great way to both destress the mind and move the body after hours of sitting at the computer learning how to code."
     puts "Here is a list of 10 yoga poses targeted at destressing the mind and body:"
     
     #poses_array = 
@@ -16,9 +17,18 @@ class CLI
   def select_pose
     input = nil
     while input != "exit"
+    puts "Please select a yoga pose 1-10 to learn more about or type 'list' to see the list of poses again or type 'exit' to exit:"
       input  = gets.strip.downcase
-    puts "Please select a yoga pose 1-10 to learn more about or type 'exit' to exit:"
-    
+      case input
+      when "1" 
+        puts "More info on pose 1:"
+      when "2" 
+        puts "More info on pose 2:" 
+      when "list"
+        list_poses
+      else 
+        puts "Invalid input, refer back to directions to find your pose!"
+    end
    end
   end
   
--- lib/yoga_poses.rb
+++ lib/yoga_poses.rb
@@ -1,9 +1,10 @@
 class YogaPose 
+  attr_accessor :name, :description
   #has a name 
   #has a description 
-  #
+  #has a url 
   
-  @@all = []
+  #@@all = []
   
   def initialize(name=nil, description=nil)
     @name = name 
@@ -12,7 +13,11 @@ class YogaPose
   end 
   
   def self.all
-    @@all 
+    #@@all 
+    pose_1 = self.new 
+    pose_1.name = "pose 1"
+    pose_1.description = "description 1"
+    
   end 
   
   
