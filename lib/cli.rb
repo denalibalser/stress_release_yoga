be95052fdac5f051a18274fb91773c7f86b07d3a
Denali Balser <denali.balser167@gmail.com>
2019-06-11 23:30:53 +0000

    user interface edits

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,18 +1,18 @@
 class CLI 
-  attr_accessor :name 
   
   def start 
-    puts "Welcome. Learning how to become a software engineer can be very stressful, not to mention life in general. It is very important to take care of your mind and body in times of stress, which is usually when you feel the least like doing so. Yoga is a great way to both destress the mind and move the body after hours of sitting at the computer learning how to code."
+    puts "Welcome. Learning how to become a software engineer can be very stressful, not to mention life in general. It is very important to take care of your mind and body in times of stress, which is usually when you feel the least like doing so. Yoga is an accessible and effective way to both destress the mind and move the body after hours of sitting at the computer learning how to code."
+    puts ""
     Scraper.scrape_website_poses
     #binding.pry
     list_poses
     select_pose
     goodbye
-    
   end 
   
   def list_poses
     puts "Here is a list of 6 yoga poses targeted at destressing the mind and body:"
+    puts ""
     @poses = YogaPose.all  
     @poses.each.with_index(1) do  |pose, i|
       puts  "#{i}. #{pose.name}"
@@ -21,8 +21,9 @@ class CLI
   
   def select_pose
     input = nil
-    while input != "exit"
-    puts "Please select a yoga pose 1-6 to learn more about or type 'list' to see the list of poses again or type 'exit' to exit:"
+    while input != "exit" 
+    puts ""
+    puts "Please select a yoga pose 1-6 to learn more about it or type 'list' to see the list of poses again or type 'exit' to exit:"
       input  = gets.strip.downcase
       
       if input.to_i > 0 
@@ -35,13 +36,16 @@ class CLI
       #elsif input < 7 
         #puts "Invalid input, refer back to directions to find your pose!"
       else 
+        puts ""
         puts "Invalid input, refer back to directions to find your pose!"
     end
    end
   end
   
   def goodbye 
-    puts "Namaste"
+    puts ""
+    puts "~Namaste~"
+    puts ""
   end 
   
 end
