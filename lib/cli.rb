f36f0e3c434f2d24eb6ad7c06f06a1795f7c6fec
Denali Balser <denali.balser167@gmail.com>
2019-06-17 16:25:35 +0000

    worked on fixing error in cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,33 +13,34 @@ class CLI
     puts "Here is a list of 6 yoga poses targeted at destressing the mind and body:"
     puts ""
     @poses = YogaPose.all  
-    @poses.each.with_index(1) do  |pose, i|
+    @poses.each.with_index(1) do |pose, i|
       puts  "#{i}. #{pose.name}"
     end 
+    #binding.pry
   end
   
   def select_pose
     input = nil
+    
     while input != "exit" 
       puts ""
       puts "Please select a yoga pose 1-6 to learn more about it or type 'list' to see the list of poses again or type 'exit' to exit:"
-      input  = gets.strip.downcase
+      input = gets.strip.downcase
       
-      if input.to_i > 0 
+      if input.to_i > 0 && input.to_i != nil
+        #binding.pry
         the_pose = @poses[input.to_i-1]
         puts "#{the_pose.name}: #{the_pose.description}"
       elsif input == "list"
         list_poses
       elsif input == "exit"
         goodbye
-      #elsif input < 7 
-        #puts "Invalid input, refer back to directions to find your pose!"
       else 
         puts ""
         puts "Invalid input, refer back to directions to find your pose!"
+      end
     end
    end
-  end
   
   def goodbye 
     puts ""
