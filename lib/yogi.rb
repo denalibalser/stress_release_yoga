6466bc3a71c5716c3e717afe8a8d2f2f0e562137
Denali Balser <46694709+denalibalser@users.noreply.github.com>
2019-09-23 11:33:52 -0400

    Update yogi.rb

--- lib/yogi.rb
+++ lib/yogi.rb
@@ -1,5 +1,3 @@
-#1. Create a Yogi class in which each instance is initialized only with a name. There should be a class method .all that returns all instances of Yogis. There should also be a  class method in Yogis called .make_three_yogis that makes three made-up Yogis. 
-
 class Yogi  
   attr_accessor :name 
   
@@ -18,13 +16,8 @@ class Yogi
     bob = Yogi.new("Bob")
     harry = Yogi.new("Harry")
     helen = Yogi.new("Helen")
-    #YogaPoses.new(name, description, bob)
-    #YogaPoses.new(name, description, harry)
-    #YogaPoses.new("Downward Dog", description, helen)
-    #helen.yoga_poses #=> returns [<YogaPose#495834059 @name = "Downdward Dog", @yogi = <Yogi#3459348503495 @name = "Helen"]>">] 
   end 
   
-  #4. In the Yogi class, write an instance method #yoga_poses that returns an array of all the yoga poses associated with this Yogi.
   def yoga_poses
     YogaPoses.all.select do |pose|
       pose.yogi == self
