3bd68a0d8761c81f888b5aedb9341a066c3c2b14
denalibalser <denali.balser167@gmail.com>
2021-04-26 10:57:10 -0600

    remove comments

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,10 @@ class Scraper
   def self.scrape_website_poses
      doc = Nokogiri::HTML(open("https://www.self.com/gallery/de-stressing-yoga-poses-to-help-you-calm-down"))
      
-    #  names = doc.css("figcaption h2.hed").map do |pose_el|
     names = doc.css("figcaption h2.gallery-slide-caption__hed span.gallery-slide-caption__hed-text").map do |pose_el|
         pose_el.text
      end
      
-    #  descriptions = doc.css("blockquote.caption div").map do |caption_el|
     descriptions = doc.css("figcaption div.gallery-slide-caption__dek-container div.gallery-slide-caption__dek div").map do |caption_el|
        caption_el.text
      end
